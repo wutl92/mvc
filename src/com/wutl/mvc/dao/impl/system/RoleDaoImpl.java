094284d0c1bb795cd709d5ea5c87c3b740153735
wutl92 <wutl92@aliyun.com>
2016-05-19 17:01:13 +0800

    ss实打实的速度

---
+++ src/com/wutl/mvc/dao/impl/system/RoleDaoImpl.java
@@ -0,0 +1,27 @@
+package com.wutl.mvc.dao.impl.system;
+
+import com.wutl.mvc.bean.Menu;
+import com.wutl.mvc.bean.Role;
+import com.wutl.mvc.dao.BaseDaoImpl;
+import com.wutl.mvc.dao.system.MenuDao;
+import com.wutl.mvc.dao.system.RoleDao;
+import com.wutl.mvc.tool.Condition;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * <pre>
+ * 名称:
+ * 功能:
+ * 作者: wutl
+ * Copyright: 杭州市威灿科技 (c) 2016
+ * 版本 1.0[原始架构]
+ * </pre>
+ */
+@Repository
+public class RoleDaoImpl extends BaseDaoImpl<Role> implements RoleDao {
+
+
+}
---
+++ src/com/wutl/mvc/dao/system/RoleDao.java
@@ -0,0 +1,18 @@
+package com.wutl.mvc.dao.system;
+
+import com.wutl.mvc.bean.Role;
+import com.wutl.mvc.dao.BaseDao;
+
+/**
+ * <pre>
+ * 名称:
+ * 功能:
+ * 作者: wutl
+ * Copyright: 杭州市威灿科技 (c) 2016
+ * 版本 1.0[原始架构]
+ * </pre>
+ */
+
+public interface RoleDao  extends BaseDao<Role> {
+
+}
